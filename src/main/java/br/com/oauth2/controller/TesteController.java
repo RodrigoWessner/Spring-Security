160032e956fef50d52b6f035906764f1103d71ac
rodrigo.wessner <rodrigowessner1@gmail.com>
2022-09-23 16:00:30 -0300

    project config

---
+++ src/main/java/br/com/oauth2/controller/TesteController.java
@@ -0,0 +1,14 @@
+package br.com.oauth2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+    @GetMapping
+    public String get(){
+        return "teste";
+    }
+}
